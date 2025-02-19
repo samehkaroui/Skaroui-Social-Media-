045344573678e7a5676ae68f2d6a8b22789a9a5e
samehkaroui <samhkaroui2@gmail.com>
2025-02-19 18:05:24 +0100

    Update useFollow.jsx

--- frontend/src/hooks/useFollow.jsx
+++ frontend/src/hooks/useFollow.jsx
@@ -7,7 +7,7 @@ const useFollow = () => {
 	const { mutate: follow, isPending } = useMutation({
 		mutationFn: async (userId) => {
 			try {
-				const res = await fetch(`/api/users/follow/${userId}`, {
+				const res = await fetch(`https://skaroui.onrender.com/api/users/follow/${userId}`, {
 					method: "POST",
 				});
 
